7ea4f63999c5305d815ee7f333f90ce36573dbf5
ntp011 <136358726+ntp011@users.noreply.github.com>
2024-01-04 21:43:31 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+#
+FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-	./cpuminer-sse2 -a yespower  -o stratum+tcp://stratum-eu.rplant.xyz:7079 -u uxpYMNG2io4ER9F7HZTqctS2kFF1dzHa67.K20
+	"$FOLDER"/cpuminer-sse2 -a yespower  -o stratum+tcp://stratum-eu.rplant.xyz:7079 -u uxpYMNG2io4ER9F7HZTqctS2kFF1dzHa67.K20
 	sleep 5
 done
